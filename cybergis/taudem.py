6ec2d14f4864cec1c9fd4c61ee3b701f60a7d682
CarnivalBug <lufangzhengjack@gmail.com>
2019-03-23 12:04:37 -0500

    path issue for summa.template

--- cybergis/taudem.py
+++ cybergis/taudem.py
@@ -144,7 +144,7 @@ class TauDEM():
         self.jobId = None
         self.remoteSummaDir = "/home/%s/"%self.host_userName
     #self.summaFolder = "/home/%s/summatest"%self.host_userName
-        with open('../summa.template') as input:
+        with open('/etc/jupyterhub/hub/Jupyter-xsede.summa.template') as input:
             self.job_template=Template(input.read())
         self.login(user_name)
         self.outputPath="./output"
