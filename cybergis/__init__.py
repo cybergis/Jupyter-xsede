2ce6ab0ad7e8d0a858b4797a58c0668b07efb0f0
CarnivalBug <lufangzhengjack@gmail.com>
2019-09-09 13:38:17 -0500

    Update __init__.py

--- cybergis/__init__.py
+++ cybergis/__init__.py
@@ -6,6 +6,7 @@ from .summa import Summa
 from .taudem import TauDEM
 from .htc import SummaHTC, JobsInfo
 from .comm import SSHComm
+from .summa_general import Summa_general
 #
 #def main():
 #    """Entry point for the application script"""
