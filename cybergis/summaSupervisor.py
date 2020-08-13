8f7a98a84037465dad08d0ff0663c0b5c4a6d83a
Zimo Xiao <xiaozimo@zuggr.com>
2020-08-13 10:56:09 -0500

    Update summaSupervisor.py

--- cybergis/summaSupervisor.py
+++ cybergis/summaSupervisor.py
@@ -8,12 +8,13 @@ from .summa import *
 from .utils import *
 from .job import *
 
-class summa_base():
+
+class summa_base:
     username = ""
     machine = "keeling"
-    model_source_folder_path = "" ## the path to the summa testcase folder
-    file_manager_path = "" ## the path to the filemanager folder
-    jobname = "summa" ## the name of the job
+    model_source_folder_path = ""  ## the path to the summa testcase folder
+    file_manager_path = ""  ## the path to the filemanager folder
+    jobname = "summa"  ## the name of the job
     wt = 10
     node = 1
     keeling_con = None
@@ -24,93 +25,120 @@ class summa_base():
     private_key_path = None
     user_pw = None
     model_name = None
-        
+
+
 class SummaSupervisorToHPC(summa_base):
-    def __init__(self, parameters,
-                 username="cigi-gisolve",
-                 private_key_path="/opt/cybergis/.gisolve.key",
-                 user_pw=None):
-        self.username=username
+    def __init__(
+        self,
+        parameters,
+        username="cigi-gisolve",
+        private_key_path="/opt/cybergis/.gisolve.key",
+        user_pw=None,
+    ):
+        self.username = username
         try:
-            self.model_name=parameters['model']
+            self.model_name = parameters["model"]
         except:
             pass
         try:
-            self.machine=parameters['machine']
+            self.machine = parameters["machine"]
         except:
             pass
         try:
-            self.file_manager_path = parameters['file_manger_rel_path']
+            self.file_manager_path = parameters["file_manger_rel_path"]
         except:
             pass
         try:
-            self.model_source_folder_path = parameters['model_source_folder_path']
+            self.model_source_folder_path = parameters["model_source_folder_path"]
         except:
             pass
         try:
-            self.workspace_path = parameters['workspace_dir']
+            self.workspace_path = parameters["workspace_dir"]
         except:
             pass
         try:
-            self.node = parameters['node']
+            self.node = parameters["node"]
         except:
             pass
         try:
-            self.wt = parameters['walltime']
+            self.wt = parameters["walltime"]
         except:
             pass
         self.private_key_path = private_key_path
         self.user_pw = user_pw
-        
+
     def submit(self, params={}):
         try:
-            self.node = params['node']
+            self.node = params["node"]
         except:
             pass
         try:
-            self.wt = params['walltime']
+            self.wt = params["walltime"]
         except:
             pass
 
         model_source_folder_path = self.model_source_folder_path
         file_manager_path = self.file_manager_path
 
-        if (self.machine == "keeling"):
-            summa_sbatch = SummaKeelingSBatchScript(int(self.wt), self.node, self.jobname)
-            sjob = SummaKeelingJob(self.workspace_path, self.connection, summa_sbatch, model_source_folder_path,
-                                   file_manager_path, name=self.jobname)
-            sjob.go()
-        elif (self.machine.lower() == "comet"):
-            summa_sbatch = SummaCometSBatchScript(str(int(self.wt)), self.node, self.jobname)
-            sjob = SummaCometJob(self.workspace_path, self.connection, summa_sbatch, model_source_folder_path,
-                                 file_manager_path, name=self.jobname)
-            sjob.go()
+        if self.machine == "keeling":
+            summa_sbatch = SummaKeelingSBatchScript(
+                int(self.wt), self.node, self.jobname
+            )
+            sjob = SummaKeelingJob(
+                self.workspace_path,
+                self.connection,
+                summa_sbatch,
+                model_source_folder_path,
+                file_manager_path,
+                name=self.jobname,
+            )
+        elif self.machine.lower() == "comet":
+            summa_sbatch = SummaCometSBatchScript(
+                str(int(self.wt)), self.node, self.jobname
+            )
+            sjob = SummaCometJob(
+                self.workspace_path,
+                self.connection,
+                summa_sbatch,
+                model_source_folder_path,
+                file_manager_path,
+                name=self.jobname,
+            )
+
+        sjob.go()
+
         return {
-            'remote_id': sjob.remote_id,
-            'remote_slurm_out_file_path': sjob.remote_slurm_out_file_path,
-            'remote_model_folder_path': sjob.remote_model_folder_path,
-            'local_job_folder_path': sjob.local_job_folder_path
+            "remote_id": sjob.remote_id,
+            "remote_slurm_out_file_path": sjob.remote_slurm_out_file_path,
+            "remote_model_folder_path": sjob.remote_model_folder_path,
+            "local_job_folder_path": sjob.local_job_folder_path,
         }
 
     def connect(self):
-        if (self.machine.lower()=="keeling"):
-            if (self.username == "cigi-gisolve"):
-                self.connection = SSHConnection("keeling.earth.illinois.edu",
-                            user_name="cigi-gisolve",
-                            key_path=self.private_key_path)
+        if self.machine.lower() == "keeling":
+            if self.username == "cigi-gisolve":
+                self.connection = SSHConnection(
+                    "keeling.earth.illinois.edu",
+                    user_name="cigi-gisolve",
+                    key_path=self.private_key_path,
+                )
             else:
-                self.connection = SSHConnection("keeling.earth.illinois.edu",
-                            user_name=self.username,
-                            user_pw=self.user_pw)
-        elif self.machine.lower()=="comet":
-            if self.username=="cigi-gisolve":
-                self.connection = SSHConnection("comet.sdsc.edu",
-                            user_name="cybergis",
-                            key_path=self.private_key_path)
+                self.connection = SSHConnection(
+                    "keeling.earth.illinois.edu",
+                    user_name=self.username,
+                    user_pw=self.user_pw,
+                )
+        elif self.machine.lower() == "comet":
+            if self.username == "cigi-gisolve":
+                self.connection = SSHConnection(
+                    "comet.sdsc.edu",
+                    user_name="cybergis",
+                    key_path=self.private_key_path,
+                )
             else:
-                self.connection = SSHConnection("comet.sdsc.edu",
-                            user_name=self.username,
-                            user_pw=self.user_pw)
+                self.connection = SSHConnection(
+                    "comet.sdsc.edu", user_name=self.username, user_pw=self.user_pw
+                )
         self.connection.login()
         return self
 
@@ -120,10 +148,12 @@ class SummaSupervisorToHPC(summa_base):
         # queue record very soon. So it is hard to tell if a run is completed or does not exist
         # So we use qstat (pbs) to monitor job status
         # get current hpc time and job status (remove line 1 and 3)
-        cmd = 'qstat {}'.format(remote_id)
+        cmd = "qstat {}".format(remote_id)
 
         try:
-            out = self.connection.run_command(cmd, line_delimiter=None, raise_on_error=True)
+            out = self.connection.run_command(
+                cmd, line_delimiter=None, raise_on_error=True
+            )
             if out is None:
                 return "UNKNOWN"
             # out = \
@@ -135,7 +165,15 @@ class SummaSupervisorToHPC(summa_base):
         except Exception as ex:
             return "ERROR"
 
-    def download(self, remote_model_folder_path, remote_slurm_out_file_path, local_job_folder_path):
-        self.connection.download(os.path.join(remote_model_folder_path, "output"),
-                                 local_job_folder_path, remote_is_folder=True)
+    def download(
+        self,
+        remote_model_folder_path,
+        remote_slurm_out_file_path,
+        local_job_folder_path,
+    ):
+        self.connection.download(
+            os.path.join(remote_model_folder_path, "output"),
+            local_job_folder_path,
+            remote_is_folder=True,
+        )
         self.connection.download(remote_slurm_out_file_path, local_job_folder_path)
