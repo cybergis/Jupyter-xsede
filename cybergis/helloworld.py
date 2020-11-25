e54841de931f7538270e0b7b703e8c0bb2cfe1f5
zhiyuli <zyli2004@gmail.com>
2020-11-25 14:00:37 -0600

    helloworld

--- cybergis/helloworld.py
+++ cybergis/helloworld.py
@@ -1,10 +1,8 @@
 import os
-import time
 
 from .keeling import KeelingJob, KeelingSBatchScript
 from .base import BaseScript
 from .utils import get_logger
-from .connection import SSHConnection
 
 logger = get_logger()
 
@@ -21,7 +19,7 @@ class HelloWorldKeelingSBatchScript(KeelingSBatchScript):
 #SBATCH --time=$walltime
 
 ## allocated hostnames
-echo $$SLURM_JOB_NODELIST
+echo "Compute node(s) assigned: $$SLURM_JOB_NODELIST"
 
 mkdir -p $remote_job_folder_path/output
 python helloworld.py $remote_model_folder_path/in.txt $remote_job_folder_path/output/out.txt
@@ -60,8 +58,6 @@ class HelloWorldKeelingJob(KeelingJob):
     sbatch_script_class = HelloWorldKeelingSBatchScript
 
     def prepare(self):
-        ## Save sbatch script and user scripts to local job or model folder
-        # Local Directory: "/Workspace/Job/Model/"
         # save SBatch script
         self.sbatch_script.generate_script(local_folder_path=self.local_job_folder_path,
                                            _additional_parameter_dict=self.to_dict())
@@ -87,51 +83,3 @@ class HelloWorldCometSBatchScript(HelloWorldKeelingSBatchScript):
 
 class HelloWorldCometJob(HelloWorldKeelingJob):
     sbatch_script_class = HelloWorldCometSBatchScript
-
-
-# def HelloWorldSubmission(workspace, mode_source_folder_path, nodes, wtime,
-#                        hpc="keeling",
-#                        key=None, user=None, passwd=None):
-#
-#     server_url = "keeling.earth.illinois.edu"
-#     user_name = "cigi-gisolve"
-#     SBatchScriptClass = HelloWorldKeelingSBatchScript
-#     JobClass = HelloWorldKeelingJob
-#
-#     if hpc == "comet":
-#         server_url = "comet.sdsc.edu"
-#         user_name = "cybergis"
-#         SBatchScriptClass = HelloWorldCometSBatchScript
-#         JobClass = HelloWorldCometJob
-#
-#     if user is not None:
-#         user_name = user
-#
-#     con = SSHConnection(server_url,
-#                                 user_name=user_name,
-#                                 key_path=key,
-#                                 user_pw=passwd)
-#
-#     sbatch = SBatchScriptClass(wtime, nodes, "helloword")
-#
-#     local_workspace_folder_path = workspace
-#     local_model_source_folder_path = mode_source_folder_path
-#     job = JobClass(local_workspace_folder_path, local_model_source_folder_path,
-#                    con, sbatch,
-#                    name="HelloWorld")
-#     job.go()
-#
-#     for i in range(600):
-#         time.sleep(3)
-#         status = job.job_status()
-#         if status == "ERROR":
-#             logger.info("Job status ERROR")
-#             break
-#         elif status == "C" or status == "UNKNOWN":
-#             logger.info("Job completed: {}; {}".format(job.local_id, job.remote_id))
-#             job.download()
-#             break
-#         else:
-#             logger.info(status)
-#     logger.info("Done")
-#     return job.local_job_folder_path
