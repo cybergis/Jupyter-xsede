8934165d9952bfc9b85c8850fd05042104825529
Zhiyu(Drew) Li <zyli2004@gmail.com>
2021-01-26 10:35:26 -0600

    Update mpi4py_hello_world.py

--- singularity_def/test_mpi4py/mpi4py_hello_world.py
+++ singularity_def/test_mpi4py/mpi4py_hello_world.py
@@ -4,5 +4,13 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 hostname = MPI.Get_processor_name()
+ver = MPI.Get_version()
+lib_ver = MPI.Get_library_version()
+lib_ver = ""
 
-print("Hello World! ({}/{}: {})".format(rank, size, hostname))
+try:
+    print("Hello World! ({}/{}: {}; Ver{}; Lib{})".format(rank, size, hostname, ver, lib_ver))
+    comm.Barrier()
+    print("Done! ({}/{}: {})".format(rank, size, hostname))
+except Exception as ex:
+    print("{} ({}/{}: {}; Ver{}; Lib{})".format(ex, rank, size, hostname, ver, lib_ver))
